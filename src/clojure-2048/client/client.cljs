1d4b991f34a5ffc01b7c2300f02fa2b855e1b8a0
Ivaylo Dankolov <IvoDankolov@gmail.com>
2014-03-25 15:32:08 +0200

    Tile generation partially working.

--- src/clojure-2048/client/client.cljs
+++ src/clojure-2048/client/client.cljs
@@ -1,8 +1,27 @@
 (ns clojure-2048.client
   (:require [clojure-2048.core :refer [move add-tile game-state]]
-            [clojure.browser.repl :as repl]))
 
-(js/setTimeout #(repl/connect "http://localhost:9000/repl") 0)
+            [clojure.browser.repl :as repl]
+
+            [clojure.string :as str]
+
+            [goog.dom.classes :as classes]))
+
+(defn get-classes [{:keys [x y value]}]
+  [".tile"
+   (str ".tile-position-" (inc x) "-" (inc y))
+   (str ".tile-" (if (<= value 2048)
+                   value
+                   "super"))])
+
+(defn make-js-tile [tile]
+  (let [js-tile (.createElement js/document "div")]
+    (doseq [cls (get-classes tile)]
+      (classes/add js-tile cls))
+    js-tile))
+
+
+(repl/connect "http://localhost:9000/repl")
 
 (def test-board
   [{:value 2 :x 0 :y 0}
@@ -20,3 +39,10 @@
    {:value 2 :x 0 :y 3}
    {:value 2 :x 1 :y 3}
    {:value 4 :x 3 :y 3}])
+
+
+(def tile-container
+  (. js/document querySelector ".tile-container"))
+
+(doseq [tile test-board]
+  (. tile-container appendChild (make-js-tile tile)))
