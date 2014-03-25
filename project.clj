3f34027912d9c56771895c453d4d8600de299e6e
Ivaylo Dankolov <IvoDankolov@gmail.com>
2014-03-25 12:30:38 +0200

    Fixed project file. Lein run now works.

--- project.clj
+++ project.clj
@@ -13,15 +13,17 @@
   :cljsbuild {
     :builds {
       :server {
-        :source-paths ["src/cljs/clojure-2048/server"]
-        :compiler {:output-to "target/server.js"
+        :source-paths ["src/clojure-2048/server"]
+        :compiler {:output-to "resources/server.js"
                    :optimizations :simple
                    :target :nodejs
                    :pretty-print true}}
 
       :client {
-        :source-paths ["src/cljs/clojure-2048/client"]
+        :source-paths ["src/clojure-2048/client"]
         :compiler {:output-to "resources/public/js/cljs.js"
                    :optimizations :simple
-                   :pretty-print true}}}})
+                   :pretty-print true}}}}
+
+  :main "resources/server.js")
 
